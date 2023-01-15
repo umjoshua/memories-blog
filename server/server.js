1260e7111b7f3ab1b0bbe8a36750bfe74214c674
Joshua Mathew <umjoshua@gmail.com>
2023-01-15 17:05:45 +0530

    server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from 'dotenv';
 
+//Routes
+import PostRoutes from './routes/Posts.js';
+
 dotenv.config();
 
 const app = express()
@@ -15,6 +18,8 @@ const MONGODB_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended:true }));
 app.use(cors())
+
+app.use('/posts',PostRoutes);
 
 mongoose.set('strictQuery',true);
 mongoose.connect(MONGODB_URL,{useNewUrlParser: true}).then(()=>{
