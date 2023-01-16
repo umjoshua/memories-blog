fc43154bd84dd188de1e0b9a89fd0a970f579881
Joshua Mathew <umjoshua@gmail.com>
2023-01-16 21:38:03 +0530

    post controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,16 +1,21 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPosts = (req,res) => {
-    res.send("Works")
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find(); 
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(409).json(error.message);
+    }
 }
 
-export const createPost = async (req,res) => {
+export const createPost = async (req, res) => {
     const body = req.body;
     const newPost = new PostMessage(body)
     try {
         await newPost.save();
         return res.status(200).json(newPost);
     } catch (error) {
-        return res.status(409).json({message: error.message})
+        return res.status(409).json({ message: error.message })
     }
 }
