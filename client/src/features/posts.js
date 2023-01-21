b7c3d40bf1b9c6565d625487a6077453a588fb90
Joshua Mathew <umjoshua@gmail.com>
2023-01-21 21:46:44 +0530

    redux update

--- client/src/features/posts.js
+++ client/src/features/posts.js
@@ -6,12 +6,18 @@ export const postSlice = createSlice({
     name: 'post',
     initialState: { value: initialState },
     reducers: {
-        Posts: (state, action) => {
+        setPosts: (state, action) => {
             state.value = action.payload
+        },
+        updatePost: (state, action) => {
+            state.value = state.value.filter((post) => post._id === action.payload._id ? action.payload : post);
+        },
+        deletePost: (state, action) => {
+            state.value = state.value.filter((post) => post._id !== action.payload)
         }
     }
 })
 
-export const { Posts } = postSlice.actions;
+export const { setPosts, updatePost, deletePost } = postSlice.actions;
 
 export default postSlice.reducer;
